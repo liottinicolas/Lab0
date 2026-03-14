ecce0d0d1a146536a0ac9ec4d8db46aef51bfa30
Lucassgu2025 <hornoslucas@gmail.com>
2026-03-14 17:14:00 -0300

    Update Turista.h

--- include/Turista.h
+++ include/Turista.h
@@ -12,10 +12,16 @@ private:
     std::string ci;
     std::string nombre;
     std::string email;
-    
     std::list<Experiencia*> experiencias;
 
 public:
+    Turista(String ci,String nombre,String email);
+    String getCi();
+    String getNombre();
+    String getEmail();
+    void setNombre(String nombre);
+    void setEmail(String email);
+    void setParticipa(const Experiencia& experiencia_agregar);
     std::string toString();
     std::set<std::string> listarExperiencias(DTFecha desde, float min, float max);
 };
