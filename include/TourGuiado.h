e813a220d0d1c72bf76dcaa0f8f6d97595a9d940
Liber117 <facu.madruga@gmail.com>
2026-03-15 22:27:23 -0300

    Tourguiado
    Creo que ya finalicé con mi parte, aunque siento que está mal.

--- include/TourGuiado.h
+++ include/TourGuiado.h
@@ -10,6 +10,23 @@ private:
     std::string agencia;
     std::set<std::string> lugaresVisitados;
 public:
+    // Constructor
+    tour(string codigoReserva, string descripcion, string agencia, <string> lugaresVisitados, int precioBase, DTFecha fecha);
+
+    // Destructor
+    ~tour();
+
+    // Setters
+    void setagencia(string agencia);
+    void setlugaresvisitados(<string> lugaresVisitados);
+    void setlugarvisitado(string lugarvisitado);
+
+    // Getters
+    string getagencia();
+    <string> getlugaresvisitados();
+    bool getestalugar(string lugar);
+    int getcantlugares();
+
     float calcularCosto() override;
 };
 
--- src/TourGuiado.cpp
+++ src/TourGuiado.cpp
@@ -1,1 +1,43 @@
 #include "TourGuiado.h"
+
+// Constructor
+TourGuiado::tour(string codigoReserva, string descripcion, string agencia, <string> lugaresVisitados, int precioBase, DTFecha fecha)
+    :Experiencia(string codigoReserva, string descripcion, int precioBase, DTFecha fecha) {
+    this->lugaresVisitados = lugaresVisitados;
+    this->agencia = agencia;
+}
+
+// Destructor
+TourGuiado::~tour()
+    :~Experiencia(){}
+
+// Setters
+void TourGuiado::setagencia(){
+    this->agencia = agencia;
+}
+
+void TourGuiado::setlugaresvisitados(){
+    this->lugaresVisitados = lugaresVisitados;
+}
+
+// Getters
+string TourGuiado::getagencia(){
+    return this->agencia;
+}
+
+<string> TourGuiado::getlugaresvisitados(){
+    return this->lugaresVisitados;
+}
+
+bool TourGuiado:: getestalugar(string lugar){
+    return (this->lugaresVisitados.find(lugar) != this->lugaresVisitados.end(lugar));
+}
+
+int TourGuiado:: getcantlugares(){
+    return this->lugaresVisitados.size;
+}
+
+// Operación abstracta
+Float TourGuiado:: calcularCosto() override {
+    return ((getPrecioBase() + 2) * getCantTuristas() * getcantlugares())
+}
