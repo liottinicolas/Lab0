96cd71a6b3868002b32559859eb73e65e18ee8c7
Victoria Ríos <vikerios14@gmail.com>
2026-03-16 20:34:28 -0300

    Alojamiento

--- include/Alojamiento.h
+++ include/Alojamiento.h
@@ -4,13 +4,33 @@
 #include <string>
 #include "Experiencia.h"
 #include "TipoRegimen.h"
+#include "DTFecha.h"
 
 class Alojamiento : public Experiencia {
 private:
     std::string hotel;
     TipoRegimen regimen;
     int cantNoches;
+
 public:
+    Alojamiento(std::string codigoReserva,
+                std::string descripcion,
+                float precioBase,
+                DTFecha fecha,
+                std::string hotel,
+                TipoRegimen regimen,
+                int cantNoches);
+
+    ~Alojamiento();
+    
+    string gethotel();
+    TipoRegimen getregimen();
+    int getcantNoches();
+
+    void sethotel(string hotel);
+    void setregimen(TipoRegimen regimen);
+    void setcantNoches(int cantNoches);
+    
     float calcularCosto() override;
 };
 
--- src/Alojamiento.cpp
+++ src/Alojamiento.cpp
@@ -1,1 +1,59 @@
 #include "Alojamiento.h"
+#include "DTFecha.h"
+#include "Experiencia.h"
+#include <set>
+#include <string>
+
+using namespace std;
+
+// Constructor
+Alojamiento::Alojamiento(string codigoReserva, string descripcion, float precioBase, DTFecha fecha,
+string hotel, TipoRegimen regimen, int cantNoches)
+    : Experiencia(codigoReserva, descripcion, precioBase, fecha){
+    this->hotel = hotel;
+    this->regimen = regimen;
+    this->cantNoches = cantNoches;
+}
+
+// Destructor
+Alojamiento::~Alojamiento(){}
+
+// Getters
+string Alojamiento::gethotel(){
+    return this->hotel;
+}
+
+TipoRegimen Alojamiento::getregimen(){
+    return this->regimen;
+}
+
+int Alojamiento::getcantNoches(){
+    return this->cantNoches;
+}
+
+// Setters
+void Alojamiento::sethotel(string hotel) {
+    this->hotel = hotel;
+}
+
+void Alojamiento::setregimen(TipoRegimen regimen) {
+    this->regimen = regimen;
+}
+
+void Alojamiento::setcantNoches(int cantNoches) {
+    this->cantNoches = cantNoches;
+}
+
+// Metodo
+float Alojamiento::calcularCosto() {
+    float res;
+
+    if (getregimen() == AllInclusive) {
+        res = (getPrecioBase() * getcantNoches()) + 10 * getcantNoches();
+    } 
+    else {
+        res = (getPrecioBase() * getcantNoches());
+    }
+
+    return res;
+}
